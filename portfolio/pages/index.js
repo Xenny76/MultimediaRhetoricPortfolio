9401df5ca4e0421ef17157ccbef3dd411fe5cb77
Xenny76 <dredford@student.neumont.edu>
2025-05-08 12:52:55 -0600

    making sections sectiony

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -2,9 +2,9 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 export default function Home() {
   return (
-    <div>
-      <Navbar/>
-      <Hero/>
-    </div>
+    <ul>
+      <li><Navbar/></li>
+      <li><Hero/></li>
+    </ul>
   );
 }
