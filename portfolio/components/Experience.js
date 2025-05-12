913a4393a14ef733d1078b61222c82e9f9a80faa
Xenny76 <dredford@student.neumont.edu>
2025-05-12 16:39:02 -0600

    43

---
+++ portfolio/components/Experience.js
@@ -0,0 +1,19 @@
+export default function Experience() {
+  return (
+    <section
+      id="experience"
+      className="h-screen flex flex-col justify-center container mx-auto px-4 bg-gray-900 text-gray-200"
+    >
+      {/* Main heading */}
+      <h2 className="text-3xl font-semibold text-center mb-8">
+        Experience
+      </h2>
+
+      <div className="mt-16 max-w-3xl mx-auto">
+        <h3 className="text-2xl font-semibold text-center mb-4">Schooling</h3>
+        <p className="text-center text-gray-400">Neumont College of Computer Science — B.S. Software Development, Graduating 2027</p>
+        <p className="text-center text-gray-400">Completed high school computer science curriculum, including Java and web development courses</p>
+      </div>
+    </section>
+  )
+}
