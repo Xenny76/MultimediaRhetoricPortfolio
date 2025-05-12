2648a37b0e487f8a835dcae8020b4e966c152909
Xenny76 <dredford@student.neumont.edu>
2025-05-12 10:14:50 -0600

    29

--- portfolio/components/About.js
+++ portfolio/components/About.js
@@ -3,7 +3,10 @@ export default function About() {
     <section id="about" className="py-16 container mx-auto">
       <h2 className="text-2xl font-bold text-center mb-4">About Me</h2>
       <p className="text-gray-500 max-w-2xl mx-auto mb-4">
-        I started my coding journey with Java, JS, and C#, building a strong foundation in software development. Throughout my time at college, I&apot;ve learned many useful skills and frameworks for the industry.
+        I started my coding journey with Java, JS, and C#, building a strong foundation in software development. Throughout my time at college, I&apos;ve learned many useful skills and frameworks for the industry.
+      </p>
+      <p className="text-gray-500 max-w-2x1 mx-auto mb-4">
+        According to the 2024 Stackoverflow survey: JS, HTML/CSS, Java, and C# are all in the top 8 of professional developers, and according to the 2024 Jetbrains State of Developer Ecosystem Report they are all within the top 9 languages.
       </p>
       <p className="text-gray-500 max-w-2xl mx-auto">
         As a driven developer, I thrive in dynamic environments where I can solve complex problems, optimize performance, and contribute to impactful projects.
